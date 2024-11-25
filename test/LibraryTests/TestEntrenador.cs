987be9b6a12ac24a809734deb2f81e557f773fd7
Joaquin Viola <JOAQUIN.VIOLA@correo.ucu.edu.uy>
2024-11-25 18:58:49 -0300

    Mas Tests

--- test/LibraryTests/TestEntrenador.cs
+++ test/LibraryTests/TestEntrenador.cs
@@ -1,34 +1,64 @@
 using Discord.WebSocket;
-using Moq;
+using Library.Contenido_Parte_II;
 using NUnit.Framework;
 using Proyecto_Pokemones_I;
+using Proyecto_Pokemones_I.Items;
 
 namespace TestLibrary;
-z
+
 [TestFixture]
 public class TestEntrenador
 {
+
     [Test]
-    [TestCase("Ash Ketchum", 12345678901234567890)]
-    [TestCase("Misty Gonzalez", 12345678901299999999)]
-    public void ConstructorConParametrosValidos(string nombre, ulong id)
+    [TestCase("Ash Ketchup", 12345678901234567890, null)]
+    [TestCase("Misty gonzalez", 12345678901234567890, null)]
+    
+    public void ConstructorTest(string nombre, ulong id, SocketGuildUser guild)
     {
-        // Crear un mock de SocketGuildUser
-        var mockGuildUser = new Mock<SocketGuildUser>();
-
-        // Configurar las propiedades simuladas que puedan ser necesarias
-        mockGuildUser.Setup(user => user.Username).Returns(nombre);
-        mockGuildUser.Setup(user => user.Id).Returns(id);
-
-        // Obtener el objeto simulado
-        var guild = mockGuildUser.Object;
-
-        // Crear una instancia de Entrenador
         Entrenador unEntrenador = new Entrenador(nombre, id, guild);
 
-        // Verificar que las propiedades se configuraron correctamente
         Assert.That(unEntrenador.GetNombre(), Is.EqualTo(nombre));
         Assert.That(unEntrenador.Id, Is.EqualTo(id));
         Assert.That(unEntrenador.GetSocketGuildUser(), Is.EqualTo(guild));
+
+    }
+    
+    [Test]
+    [TestCase("Ash Ketchup", 12345678901234567890, null)]
+    [TestCase("Misty gonzalez", 12345678901234567890, null)]
+
+    public void RecargarItemsTest(string nombre, ulong id, SocketGuildUser guild)
+    {
+        Entrenador unEntrenador = new Entrenador(nombre, id, guild);
+        Assert.That(unEntrenador.GetListaDeItems(), Is.EqualTo("Super Pocion (x4) / Revivir (x1) / Cura total (x2) /"));
+
+
+    }
+    
+
+    
+    /*[Test]
+    [TestCase(charmander)]
+    [TestCase(pikachu)]
+    
+
+    public void AñadirASeleccionTest(Pokemon pokemon)
+    {
+        Entrenador unEntrenador = new Entrenador("nombre", 23123131, null);
+        unEntrenador.AñadirASeleccion(pokemon);
+        Assert.That(unEntrenador.ListaDePokemones(), Is.EqualTo(pokemon.GetNombre()));
+
+    }*/
+    [Test]
+    [TestCase("Ash Ketchup", 12345678901234567890, null)]
+    [TestCase("Misty gonzalez", 12345678901234567890, null)]
+
+    public void TestDeListaDePokemones(string nombre, ulong id, SocketGuildUser guild)
+    {
+        Entrenador unEntrenador = new Entrenador(nombre, id, guild);
+        Assert.That(unEntrenador.GetListaDeItems(), Is.EqualTo("Super Pocion (x4) / Revivir (x1) / Cura total (x2) /"));
+
+
     }
 }
